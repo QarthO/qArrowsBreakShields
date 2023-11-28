5d0bf45f1402ef710d3b1d7977f72664d767b390
QarthO <qartho@gmail.com>
2023-11-28 17:10:12 -0500

    renamed plugin

--- src/main/java/gg/quartzdev/qarrowsdisableshields/qArrowsDisableShields.java
+++ src/main/java/gg/quartzdev/qarrowsdisableshields/qArrowsDisableShields.java
@@ -1,23 +1,16 @@
-package gg.quartzdev.qshieldconfig;
+package gg.quartzdev.qarrowsdisableshields;
 
+import gg.quartzdev.qarrowsdisableshields.listeners.ArrowHitPlayerListener;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class qArrowsDisableShields extends JavaPlugin {
 
-    private static qArrowsDisableShields instance;
-
-    public static qArrowsDisableShields getInstance(){
-        return instance;
-    }
-
     @Override
     public void onEnable() {
-        instance = this;
         int pluginId = 20414;
         Metrics metrics = new Metrics(this, pluginId);
-        this.getServer().getPluginManager().registerEvents(new ShieldHitListener(), this);
-
+        this.getServer().getPluginManager().registerEvents(new ArrowHitPlayerListener(), this);
     }
 
     @Override
